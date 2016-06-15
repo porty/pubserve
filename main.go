89e3b60bd756678a199619e8ccb83f0b817336f8
Robert McNeil <pppppppman@gmail.com>
2016-06-15 16:27:11 +1000

    Print listen addresses and what dir is being served

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type logWrapper struct {
@@ -15,20 +19,36 @@ func (l logWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	l.handler.ServeHTTP(w, r)
 }
 
+func printInterfaces(port int) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Print("Can't figure out your IP addresses: " + err.Error())
+		return
+	}
+	for _, addr := range addrs {
+		parts := strings.Split(addr.String(), "/")
+		if len(parts) == 2 {
+			log.Printf("Listening on http://%s:%d/", parts[0], port)
+		}
+	}
+}
+
 func main() {
 	dir := os.Getenv("DIR")
 	if dir == "" {
 		dir = os.ExpandEnv("${HOME}/Public")
 	}
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8888"
+	portStr := os.Getenv("PORT")
+	port, _ := strconv.Atoi(portStr)
+	if port == 0 {
+		port = 8888
 	}
 
 	fs := http.FileServer(http.Dir(dir))
 	http.Handle("/", logWrapper{fs})
-	log.Printf("Serving on http://0.0.0.0:%s/\n", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	log.Print("Serving directory " + dir)
+	printInterfaces(port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		panic(err)
 	}
 }
